63876d5f425cda587b92bb5e355021133113ff4c
Patriotic20 <pterest160@gmail.com>
2026-02-25 23:11:01 +0500

    chore: Update CORS configuration to replace wildcard origins and methods with specific values.

--- backend/app/main.py
+++ backend/app/main.py
@@ -34,10 +34,10 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173", 
         "http://127.0.0.1:5173", 
-        "*"
+        "https://test.api.nsumt.uz"
     ],
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["*"],
 )
 
